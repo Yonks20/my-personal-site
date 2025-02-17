d16e6c8be894eefe8d8e9da574bd4c827d72e82a
Yonks20 <yonkieyudha@gmail.com>
2025-02-17 21:15:14 +0700

    Done dev

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Typed from "typed.js"
+
+const typed = new Typed("#heading", {
+    strings: ["UI UX Designer", "Engineering Mentor", "Product Design", "Product Engineer"],
+    loop: true,
+    typeSpeed: 70
+})
